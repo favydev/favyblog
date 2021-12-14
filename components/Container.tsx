c59198cf8e52d8f42822d24e8fbd1dd361a0e9c6
git-yj <1024byte.code@gmail.com>
2021-12-14 12:01:56 +0530

    changes on name of meta tags

--- components/Container.tsx
+++ components/Container.tsx
@@ -51,8 +51,8 @@ export default function Container(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
-        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta property="og:url" content={`https://yashjainsite.vercel.app${router.asPath}`} />
+        <link rel="canonical" href={`https://yashjainsite.vercel.app${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Yash Jain" />
         <meta property="og:description" content={meta.description} />
