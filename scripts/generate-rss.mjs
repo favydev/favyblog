7a7271b40876bc09a3b95a2cb5d520dbc3a711d2
git-yj <1024byte.code@gmail.com>
2021-12-14 12:33:52 +0530

    favicons and rss changes

--- scripts/generate-rss.mjs
+++ scripts/generate-rss.mjs
@@ -4,15 +4,15 @@ import { allBlogs } from '.contentlayer/data';
 
 async function generate() {
   const feed = new RSS({
-    title: 'Lee Robinson',
-    site_url: 'https://leerob.io',
-    feed_url: 'https://leerob.io/feed.xml'
+    title: 'Yash Jain',
+    site_url: 'https://yashjaing.com',
+    feed_url: 'https://yashjaing.com/feed.xml'
   });
 
   allBlogs.map((post) => {
     feed.item({
       title: post.title,
-      url: `https://leerob.io/blog/${post.slug}`,
+      url: `https://yashjaing.com/blog/${post.slug}`,
       date: post.publishedAt,
       description: post.summary
     });
