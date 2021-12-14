e35b844989249c78dcb99113f1ff5c766806bbd7
git-yj <1024byte.code@gmail.com>
2021-12-14 13:13:59 +0530

    removed veiws counter from /blog

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import useSWR from 'swr';
 
 import fetcher from 'lib/fetcher';
-import { Views } from 'lib/types';
 import type { Blog } from '.contentlayer/types';
 
 export default function BlogPost({
@@ -10,8 +9,7 @@ export default function BlogPost({
   summary,
   slug
 }: Pick<Blog, 'title' | 'summary' | 'slug'>) {
-  const { data } = useSWR<Views>(`/api/views/${slug}`, fetcher);
-  const views = data?.total;
+  
 
   return (
     <Link href={`/blog/${slug}`}>
@@ -22,7 +20,7 @@ export default function BlogPost({
               {title}
             </h4>
             <p className="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0">
-              {`${views ? new Number(views).toLocaleString() : '–––'} views`}
+              
             </p>
           </div>
           <p className="text-gray-600 dark:text-gray-400">{summary}</p>
