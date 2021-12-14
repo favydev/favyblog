44b368d567f19514d9757b18a65265c310316993
git-yj <1024byte.code@gmail.com>
2021-12-14 13:43:51 +0530

    design changes

--- components/Container.tsx
+++ components/Container.tsx
@@ -46,7 +46,7 @@ export default function Container(props) {
   };
 
   return (
-    <div className="bg-gray-50 dark:bg-gray-900">
+    <div className=" bg-gray-0 dark:bg-gray-900">
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
@@ -67,8 +67,8 @@ export default function Container(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <div className="flex flex-col justify-center px-8">
-        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+      <div className="flex flex-col justify-center px-5">
+        <nav className="flex items-center justify-between w-full relative max-w-full border-gray-200 dark:border-gray-700 mx-auto pt-5 pb-8 sm:pb-16  text-gray-900 bg-gray-0  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
           <a href="#skip" className="skip-nav">
             Skip to content
           </a>
@@ -116,7 +116,7 @@ export default function Container(props) {
       </div>
       <main
         id="skip"
-        className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900"
+        className="flex flex-col justify-center px-8 bg-gray-0 dark:bg-gray-900"
       >
         {children}
         <Footer />
--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -46,7 +46,7 @@ export default function Subscribe() {
   };
 
   return (
-    <div className="border border-blue-200 rounded p-6 my-4 w-full dark:border-gray-800 bg-blue-50 dark:bg-blue-opaque">
+    <div className="border border-blue-200 rounded-lg p-6 my-4 w-full dark:border-gray-700 bg-blue-50 dark:bg-gray-800">
       <p className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
         Subscribe to the newsletter
       </p>
@@ -62,10 +62,10 @@ export default function Subscribe() {
           type="email"
           autoComplete="email"
           required
-          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 pr-32"
+          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 pr-32"
         />
         <button
-          className="flex items-center justify-center absolute right-1 top-1 px-4 pt-1 font-medium h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
+          className="flex items-center justify-center absolute right-1 top-1 px-4 pt-1 font-medium h-8 bg-gray-100 dark:bg-gray-600 text-gray-900 dark:text-gray-100 rounded w-28"
           type="submit"
         >
           {form.state === Form.Loading ? <LoadingSpinner /> : 'Subscribe'}
