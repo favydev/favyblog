7bd25afce03da0903aad67458c0df31c374a3306
git-yj <1024byte.code@gmail.com>
2021-12-22 08:26:51 +0530

    i mainly hangout on twitter fix:

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <h3 className="font-medium text-lg md:text-xl tracking-tight mb-4 mt-20 text-black dark:text-white w-full">
         What I'm doing now?
         </h3>
-        <p className="text-gray-600 dark:text-gray-400 mb-4">I share my ideas almost every day. My mainly hangout on Twitter. But 
+        <p className="text-gray-600 dark:text-gray-400 mb-4">I share my ideas almost every day. I mainly hangout on Twitter. But 
         this is my favourite place. Every now and then I write and share my thoughts Here.
         <br /> <br />
         Apart from that, Currenly I'm building Favy. It is on early development stage and we are excited for it. The journey is fabulous.
