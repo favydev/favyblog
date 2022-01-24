737c801fcfbbb5c6194333de7f5673c2fd7a1da7
git-yj <1024byte.code@gmail.com>
2022-01-24 18:54:47 +0530

    ff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,88 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Container from '../components/Container';
+import { useState } from 'react';
 
-export default function Home() {
+import Container from 'components/Container';
+import BlogPost from 'components/BlogPost';
+import { InferGetStaticPropsType } from 'next';
+import { pick } from 'lib/utils';
+import { allBlogs } from '.contentlayer/data';
+
+export default function Blog({
+  posts
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [searchValue, setSearchValue] = useState('');
+  const filteredBlogPosts = posts
+    .sort(
+      (a, b) =>
+        Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
+    )
+    .filter((post) =>
+      post.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
   return (
-    <Container>
-      <div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
-        <div className="flex sm:flex-row justify-between w-full">
-          <div className="flex flex-col pr-2 justify-start">
-            <h1 className="font-semibold text-2xl tracking-tight text-black dark:text-white">
-              Yash Jain
-            </h1>
-            <h2 className="text-gray-600 dark:text-gray-400 text-base font-medium">
-              Thinker, Designer <br />& Developer, He/him.
-            </h2>
-            
-          </div>
-          <div className="w-[82px] w-min-[82px]">
-            <Image
-              alt="Yash Jain"
-              height={82}
-              width={82}
-              src="/avatar.jpg"
-              className="rounded-full filter grayscale"
-            />
-          </div>
-        </div>
-        
-        <h3 className="font-medium text-lg md:text-xl tracking-tight mb-4 mt-20 text-black dark:text-white w-full">
-        What I'm doing now?
-        </h3>
-        <p className="text-gray-600 dark:text-gray-400 mb-4">I share my ideas almost every day. I mainly hangout on Twitter. But 
-        this is my favourite place. Every now and then I write and share my thoughts Here.
-        <br /> <br />
-        Apart from that, Currenly I'm building Favy. It is on early development stage and we are excited for it. The journey is fabulous.
-        <br /><br />
-        Currenly I'm also reading{' '}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://minimalistentrepreneur.com/book"
-          className="text-blue-500 leading-7 rounded-lg font-medium underline transition-all h-6"
-        >
-          the Minimalist Entrepreneur
-      
-        </a>
-        {' '}by Sahil Lavingia. I'm loving it.
-        <br /><br />
-        <Link href="/blog">
-        <a className="text-blue-500 leading-7 rounded-lg font-medium underline transition-all h-6">Here</a></Link>
-        {' '}are all Articles I've written so far. Have fun Navigating around this small garden.
+    <Container
+      title="Blog – Yash Jain"
+      description="I write about everything that interests me. Self Improvement, psychology, philosophy, startups and tech."
+    >
+      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+        <h1 className="mb-6 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+          Blog
+        </h1>
+        <p className="mb-4 text-gray-600 dark:text-gray-400">
+          {`Writing always helps me comb and find gaps in my thoughts. Here I think loud about Self Improvement, psychology, philosophy, and startups.
+            Alongwith anything that Interests me. In total, I've written ${posts.length} articles on my blog.`}
         </p>
-        
-        <span className="h-8" />
+        <div className="relative w-full mb-4">
+          <input
+            aria-label="Search articles"
+            type="text"
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search articles"
+            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+          />
+          <svg
+            className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
+        {!searchValue && (
+          <>
+            
+          </>
+        )}
+        <h3 className="mt-8 mb-3 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+
+        </h3>
+        {!filteredBlogPosts.length && (
+          <p className="mb-4 text-gray-600 dark:text-gray-400">
+            No posts found.
+          </p>
+        )}
+        {filteredBlogPosts.map((post) => (
+          <BlogPost key={post.title} {...post} />
+        ))}
       </div>
     </Container>
   );
+}
+
+export function getStaticProps() {
+  const posts = allBlogs.map((post) =>
+    pick(post, ['slug', 'title', 'summary', 'publishedAt', 'image'])
+  );
+
+  return { props: { posts } };
 }
